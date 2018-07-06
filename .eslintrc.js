ba45b910f3b08c6632c9ca3df1e79aa1a8e0e0ac
Quinn Shanahan <q.shanahan@gmail.com>
2018-07-06 09:38:59 -0400

    init

---
+++ .eslintrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+    "parserOptions": {
+        "ecmaFeatures": {
+            "jsx": true
+        },
+        "ecmaVersion": 2018,
+        "sourceType": "module"
+    },
+    "plugins": [
+        "react"
+    ],
+    "rules": {
+        "no-extra-parens": ["error", "all", { ignoreJSX: "none" }],
+    }
+};
